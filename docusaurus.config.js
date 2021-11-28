6a23d413ab6ea76cb844088ac47e4c4b14515c77
海超 <haichao@66rpg.com>
2021-11-28 20:25:02 +0800

    feat: update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,8 +13,8 @@ const config = {
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "haichao", // Usually your GitHub org/user name.
+  projectName: "my", // Usually your repo name.
 
   presets: [
     [
